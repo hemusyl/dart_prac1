527905d7c060f54b9bff3cb2d9c58b9c94856c31
Hp <humayun1278@gmail.com>
2025-03-15 16:20:30 -0400

    Merge remote-tracking branch 'origin/master'

--- loop_prac1.dart
+++ loop_prac1.dart
@@ -6,11 +6,16 @@ void main() {
     print("$z");
   }
 
+  //Example 2
+   for(int i=1; i<=10;i++){
+     print('15 x $i : ${15*(i)}');
+   }
+
   List<String>students = ['Humayun','Kabir','Zayan','Shahina'];
   print('Students 2 index Name: ${students[2]}');
 
   for(int index =0; index<=students.length; index++){
-    print('Students $index name : ${students[index]} ');
+    print('Students $index name : ${students[index]}');
   }
 }
 
