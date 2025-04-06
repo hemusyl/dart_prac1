88108b2673c04077ab53415097a5c660965067dd
Hp <humayun1278@gmail.com>
2025-04-06 23:17:04 +0600

    Merge remote-tracking branch 'origin/master'

---
+++ dart_ex_1.dart
@@ -0,0 +1,26 @@
+void main() {
+  // Sample input
+  List<Map<String, dynamic>> students = [
+    {"name": "Alice", "scores": [85, 90, 78]},
+    {"name": "Bob", "scores": [88, 76, 95]},
+    {"name": "Charlie", "scores": [90, 92, 85]}
+  ];
+
+  // Create a map of student
+  Map<String, double> studentAverages = {};
+
+  for (var student in students) {
+    String name = student["name"];
+    List<int> scores = List<int>.from(student["scores"]);
+    double average = scores.reduce((a, b) => a + b) / scores.length;
+    studentAverages[name] = average;
+  }
+
+  // Sort the map by average score
+  var sortedEntries = studentAverages.entries.toList()
+    ..sort((a, b) => b.value.compareTo(a.value));
+  Map<String, double> sortedAverages = Map.fromEntries(sortedEntries);
+
+
+  print(sortedAverages);
+}
